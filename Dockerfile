f3a0d4c89d5b9e84f94350e1114b4b7ff01e933e
Mastangithub123 <mastanazure.123@gmail.com>
2024-03-14 12:55:35 +0530

    code push

---
+++ Dockerfile
@@ -0,0 +1,70 @@
+# Use a Maven image as the base image for the builder stage
+FROM maven:3.8.4-openjdk-8-slim AS builder
+
+# Set working directory
+WORKDIR /app
+
+# Copy only the necessary files for dependency resolution
+COPY lib /app/lib
+COPY build.sh /app/build.sh
+
+# Make sure the build script is executable
+RUN chmod +x /app/build.sh
+
+# Run the build script to install dependencies
+RUN /app/build.sh
+
+# Use a smaller base image for the final image
+FROM maven:3.9.6-amazoncorretto-8-al2023
+
+# Set working directory
+WORKDIR /app
+
+# Copy the .m2 folder with resolved dependencies from the builder stage
+COPY --from=builder /root/.m2 /root/.m2
+
+
+# Expose any necessary ports
+EXPOSE 8080
+
+# Define the startup command
+CMD ["java", "-jar", "your-application.jar"]
+
+
+
+
+
+######  APPROACH 2 ##############################
+######################################################################################
+
+# # Use a Maven image as the base image
+# FROM maven:3.9.6-amazoncorretto-8-al2023 AS builder
+
+# # Set working directory
+# WORKDIR /app
+
+# # Copy only the necessary files for dependency resolution
+# COPY lib /app/lib
+# COPY build.sh /app/build.sh
+
+
+# # Make sure the build script is executable
+# RUN chmod +x /app/build.sh
+
+# # Run the build script to install dependencies
+# RUN /app/build.sh
+
+# # Use a smaller base image for the final image
+# FROM maven:3.9.6-amazoncorretto-8-al2023
+
+# # Copy the .m2 folder with resolved dependencies from the builder stage
+# COPY --from=builder /root/.m2 /root/.m2
+
+# # Set working directory
+# WORKDIR /app
+
+# # Copy the entire application
+# COPY . .
+
+# # Now you can use this image in your pipeline to build other projects
+
---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Move to the jar folder
+cd lib
+
+
+# Install additional JAR files to your local Maven repository
+mvn install:install-file -Dfile=memcached-1.0.jar -DgroupId=com.danga.MemCached -DartifactId=memcached -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=notifier.zip -DgroupId=notifier -DartifactId=notifier -Dversion=11.1.0.jbossorg-1 -Dpackaging=jar.
+mvn install:install-file -Dfile=prime-validation-1.5.jar -DgroupId=com.travelex.prime -DartifactId=prime-validation -Dversion=1.5 -Dpackaging=jar -e -X
+mvn install:install-file -Dfile=prime-config-1.1.jar -DgroupId=com.travelex.prime -DartifactId=prime-config -Dversion=1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=sac-1.3.jar -DgroupId=sac -DartifactId=sac -Dversion=1.3 -Dpackaging=jar
+mvn install:install-file -Dfile=wire-bank-client-1.0.1.jar -DgroupId=com.travelex.wire -DartifactId=wire-bank-client -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=wire-rate-client-1.0.1.jar -DgroupId=com.travelex.wire -DartifactId=wire-rate-client -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=wire-corridor-client-1.0.1.jar -DgroupId=com.travelex.wire -DartifactId=wire-corridor-client -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=wire-order-client-1.0.1.jar -DgroupId=com.travelex.wire -DartifactId=wire-order-client -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=wire-sender-client-1.0.1.jar -DgroupId=com.travelex.wire -DartifactId=wire-sender-client -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=wire-beneficiary-client-1.0.1.jar -DgroupId=com.travelex.wire -DartifactId=wire-beneficiary-client -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=wire-client-common-1.0.1.jar -DgroupId=com.travelex.wire -DartifactId=wire-client-common -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=javax.jms-1.1.jar -DgroupId=javax.jms -DartifactId=jms -Dversion=1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=nekohtml-1.9.12.jar -DgroupId=nekohtml -DartifactId=nekohtml -Dversion=1.9.12 -Dpackaging=jar
+mvn install:install-file -Dfile=je-3.3.62.jar -DgroupId=com.sleepycat -DartifactId=je -Dversion=3.3.62 -Dpackaging=jar
+mvn install:install-file -Dfile=xercesImpl-2.9.1.jar -DgroupId=xercesImpl -DartifactId=xercesImpl -Dversion=2.9.1 -Dpackaging=jar
+mvn install:install-file -Dfile=xalan-2.7.1.jar -DgroupId=xalan -DartifactId=xalan -Dversion=2.7.1 -Dpackaging=jar
+mvn install:install-file -Dfile=serializer-2.7.1.jbossorg-1.jar -DgroupId=xalan -DartifactId=serializer -Dversion=2.7.1.jbossorg-1 -Dpackaging=jar
+mvn install:install-file -Dfile=xalan-2.7.1.jbossorg-1.jar -DgroupId=xalan -DartifactId=xalan -Dversion=2.7.1.jbossorg-1 -Dpackaging=jar
+mvn install:install-file -Dfile=serializer-2.7.1.jbossorg-1.jar -DgroupId=xalan -DartifactId=serializer -Dversion=2.7.1.jbossorg-1 -Dpackaging=jar
+mvn install:install-file -Dfile=xercesImpl-2.9.1-jbossas-1.jar -DgroupId=xerces -DartifactId=xercesImpl -Dversion=2.9.1-jbossas-1 -Dpackaging=jar
+mvn install:install-file -Dfile=saaj-impl-1.3.16-jbossorg-1.jar -DgroupId=com.sun.xml.messaging -DartifactId=saaj-impl -Dversion=1.3.16-jbossorg-1 -Dpackaging=jar
+
+# #Build source code
+# # Debugging: Print current directory
+# echo "Current directory: $(pwd)"
+# cd ..
+# mvn clean install -U -Dmaven.test.skip=true
+
